828ae636d995caa2449d70054fe041f8302557e6
monzag <zaganiacz.m@gmail.com>
2018-02-06 22:44:09 +0100

    Remove items1D field

--- src/main/java/com/monzag/Flattener.java
+++ src/main/java/com/monzag/Flattener.java
@@ -7,20 +7,19 @@ import java.util.stream.Collectors;
 
 public class Flattener {
 
-    public LinkedList<Object> items1D = new LinkedList<>();
-
-    public void flatteningMultiDList(List<Object> items) {
+    public LinkedList<Object> flatteningMultiDList(List<Object> items, LinkedList<Object> items1D) {
         for (Object item : items) {
             if (item instanceof List) {
-                flatteningMultiDList((List) item);
+                flatteningMultiDList((List) item, items1D);
 
             } else if (item.getClass().isArray()) {
                 List<Object> listFromArray = Arrays.stream((Object[]) item).collect(Collectors.toList());
-                flatteningMultiDList(listFromArray);
+                flatteningMultiDList(listFromArray, items1D);
 
             } else {
                 items1D.add(item);
             }
         }
+        return items1D;
     }
 }
