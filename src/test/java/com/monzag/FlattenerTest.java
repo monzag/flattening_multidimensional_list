106fbe124aeed22bbba3d089c843429d30e4aa33
monzag <zaganiacz.m@gmail.com>
2018-02-06 22:00:47 +0100

    Add testConstructor

--- src/test/java/com/monzag/FlattenerTest.java
+++ src/test/java/com/monzag/FlattenerTest.java
@@ -1,7 +1,16 @@
 package com.monzag;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlattenerTest {
+
+    @Test
+    void testConstructor() {
+        Flattener flattener = new Flattener();
+    }
+
 
 }
