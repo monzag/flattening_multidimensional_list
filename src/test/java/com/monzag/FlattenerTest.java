a143fbf02c58493efa79db7c8529cf84c3792aa6
monzag <zaganiacz.m@gmail.com>
2018-02-06 22:04:13 +0100

    Add field items

--- src/test/java/com/monzag/FlattenerTest.java
+++ src/test/java/com/monzag/FlattenerTest.java
@@ -3,19 +3,24 @@ package com.monzag;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlattenerTest {
 
     private Flattener flattener;
+    private LinkedList<Object> items;
 
     @BeforeEach
     void setup() {
         flattener = new Flattener();
+        items = new LinkedList<>();
     }
-    
+
     @Test
     void testConstructor() {
         Flattener flattener = new Flattener();
     }
+
 }
