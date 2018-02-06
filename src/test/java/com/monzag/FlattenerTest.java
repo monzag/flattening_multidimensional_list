e0e64be5078e1f4faf18165d62ede470a2268e7d
monzag <zaganiacz.m@gmail.com>
2018-02-06 22:31:46 +0100

    Add private method addNumbersAndStringsToItems

--- src/test/java/com/monzag/FlattenerTest.java
+++ src/test/java/com/monzag/FlattenerTest.java
@@ -28,9 +28,7 @@ class FlattenerTest {
 
     @Test
     void testFlattening1DList() {
-        items.add(1);
-        items.add("Test");
-        items.add(20.1);
+        addNumbersAndStringsToItems();
         String expect = "[1, Test, 20.1]";
         String actual = flattener.flatteningMultiDList(items, items1D).toString();
         assertEquals(expect, actual);
@@ -38,12 +36,17 @@ class FlattenerTest {
 
     @Test
     void testFlattening2DList() {
-        items.add(1);
-        items.add("Test");
+        addNumbersAndStringsToItems();
         Integer[] years = {1997, 2015};
         items.add(years);
-        String expect = "[1, Test, 1997, 2015]";
+        String expect = "[1, Test, 20.1, 1997, 2015]";
         String actual = flattener.flatteningMultiDList(items, items1D).toString();
         assertEquals(expect, actual);
+    }
+
+    private void addNumbersAndStringsToItems() {
+        items.add(1);
+        items.add("Test");
+        items.add(20.1);
     }
 }
